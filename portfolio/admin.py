f7d65b8fcfba7ecec2edab3ea9b65d716cc56f2e
katasrofi <danielsensei42@gmail.com>
2025-01-13 10:50:30 +0700

    Add the models into admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,11 @@
+# Django Library
 from django.contrib import admin
 
+# My models 
+from .models import Profile, Project, Skill, Testimonial
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Testimonial)
