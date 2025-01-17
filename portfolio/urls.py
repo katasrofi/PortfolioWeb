7b63de346e5e058be37c889e08c73b2741fae075
katasrofi <danielsensei42@gmail.com>
2025-01-17 20:56:51 +0700

    add url into project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,13 @@ from django.urls import path
 # Views function
 from . import views 
 
+# App name 
+app_name = 'portfolio'
+
 # The Urls 
 urlpatterns = [
-        path('', views.Home.as_view(), name='home')
+        path('', views.Home.as_view(), name='home'),
+        path('project/<int:pk>/', views.ProjectPortfolio.as_view(), name='project'),
+        #path('project/<int:pk>/', views.ProjectPage, name='project'),
+
         ]
