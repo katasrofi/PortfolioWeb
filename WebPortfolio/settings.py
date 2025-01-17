4a2c8b29390fe8e2801cce4a3739dd96ad12f132
katasrofi <danielsensei42@gmail.com>
2025-01-17 20:52:49 +0700

    Add media dirs and media root for image upload

--- WebPortfolio/settings.py
+++ WebPortfolio/settings.py
@@ -125,7 +125,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Upload File 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
