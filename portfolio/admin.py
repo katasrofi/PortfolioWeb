93327a47ef0d26da4aeae6d43622c8d3cbba106c
katasrofi <danielsensei42@gmail.com>
2025-01-20 12:29:36 +0700

    Create Custom admin for project and skill

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -4,8 +4,20 @@ from django.contrib import admin
 # My models 
 from .models import Profile, Project, Skill, Testimonial
 
+# Customize the admin panel 
+class ProjectInLine(admin.StackedInline):
+    model = Skill
+    extra = 3
+
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ('Project Title', {'fields': ['title']}),
+            ('More Information', {'fields': ['summary', 'description', 'image', 'url'], 'classes': ['collapse']}),
+            ]
+    inlines = [ProjectInLine]
+
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Skill)
 admin.site.register(Testimonial)
